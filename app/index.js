ae1fad47520f66239f67fe26f9f7a64acf084bbe
Alexander Fomin <websash@gmail.com>
2016-05-18 17:06:51 +0300

    Moved routes to separate module

--- app/index.js
+++ app/index.js
@@ -1,41 +1,29 @@
-if (process.env.NODE_ENV !== 'production') require('./styles/main.css')
+if (process.env.NODE_ENV !== 'production')
+  require('./styles/main.css')
+
 import React from 'react'
 import {render} from 'react-dom'
-import {Router, Route, browserHistory, IndexRoute} from 'react-router'
-import {syncHistoryWithStore} from 'react-router-redux'
+import {Router, browserHistory} from 'react-router'
+// import {syncHistoryWithStore} from 'react-router-redux'
 import {Provider} from 'react-redux'
-import App from './containers/App'
-import Login from './containers/Login'
-import Questions from './containers/Questions'
-import Answers from './containers/Answers'
-import AskQuestion from './containers/AskQuestion'
-import User from './containers/User'
-import NoMatch from './components/NoMatch'
-import store from './store'
+import configureStore from './store'
+import configureRoutes from './routes'
 
-const history = syncHistoryWithStore(browserHistory, store)
+let appState
+try {
+  appState = JSON.parse(unescape(window.__APP_STATE__))
+} catch (_) {}
 
-function requireAuth(nextState, replace) {
-  if (!store.getState().auth.isAuthenticated) {
-    replace({
-      pathname: '/login',
-      state: {nextPathname: nextState.location.pathname}
-    })
-  }
-}
+if (process.env.NODE_ENV !== 'production')
+  console.info('__APP_STATE__', appState)
+
+const store = configureStore(appState, browserHistory)
+const routes = configureRoutes(store)
 
 render(
   <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Questions} />
-        <Route path="/login" component={Login} />
-        <Route path="/questions/ask" onEnter={requireAuth} component={AskQuestion} />
-        <Route path="/questions/:slugId" component={Answers} />
-        <Route path="/questions/show/:filter" component={Questions} />
-        <Route path="/users/:username" component={User} />
-        <Route path="*" component={NoMatch} />
-      </Route>
-    </Router>
+    {/* syncHistoryWithStore(browserHistory, store) not sure I need this */}
+    <Router routes={routes} history={browserHistory} />
   </Provider>,
-  document.getElementById('app'))
+  document.getElementById('app')
+)
