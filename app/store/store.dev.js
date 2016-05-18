732c0758fec9fce60ab7cfb34dc441df41490300
Alexander Fomin <websash@gmail.com>
2016-05-18 17:03:32 +0300

    Updated the store module to be configured with initialState

--- app/store/store.dev.js
+++ app/store/store.dev.js
@@ -1,16 +1,22 @@
 import {applyMiddleware, createStore} from 'redux'
-import {routerMiddleware} from 'react-router-redux'
-import {browserHistory} from 'react-router'
 import thunk from 'redux-thunk'
+import {routerMiddleware} from 'react-router-redux'
 import reducer from '../reducers'
 import api from '../middleware/api'
 import logger from '../middleware/logger'
 
-const store = createStore(
-  reducer,
-  applyMiddleware(
-    thunk, routerMiddleware(browserHistory), api, logger
+export default (initialState, history) => {
+  const store = createStore(
+    reducer, initialState,
+    applyMiddleware(thunk, routerMiddleware(history), api, logger)
   )
-)
 
-export default store
+  // if (module.hot) {
+  //   module.hot.accept('../reducers', () => {
+  //     const nextRootReducer = require('../reducers').default
+  //     store.replaceReducer(nextRootReducer)
+  //   })
+  // }
+
+  return store
+}
--- app/store/store.prod.js
+++ app/store/store.prod.js
@@ -1,13 +1,11 @@
 import {applyMiddleware, createStore} from 'redux'
-import {routerMiddleware} from 'react-router-redux'
-import {browserHistory} from 'react-router'
 import thunk from 'redux-thunk'
+import {routerMiddleware} from 'react-router-redux'
 import reducer from '../reducers'
 import api from '../middleware/api'
 
-const store = createStore(
-  reducer,
-  applyMiddleware(thunk, routerMiddleware(browserHistory), api)
-)
-
-export default store
+export default (initialState, history) =>
+  createStore(
+    reducer, initialState,
+    applyMiddleware(thunk, routerMiddleware(history), api)
+  )
