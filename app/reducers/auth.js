c32e35070b966ea4070ecc618a814a236fb2a4fb
Alexander Fomin <websash@gmail.com>
2016-05-18 16:56:30 +0300

    updated reducers; init username is coming from cookie in the auth reducer

--- app/reducers/auth.js
+++ app/reducers/auth.js
@@ -1,27 +1,25 @@
 import * as t from '../constants'
+import {getCookie} from '../utils'
 
 export default function auth(state = {
-  isFetching: false,
-  // need a util to check if the token is expired
-  isAuthenticated: !!localStorage.getItem('usertoken'),
-  username: localStorage.getItem('username') || null
+  isFetching: false, username: getCookie('username'), isLoggedIn: !!getCookie('username')
 }, action) {
   switch (action.type) {
     case t.LOGIN_REQUEST:
-      return {...state, isFetching: true, isAuthenticated: false}
+      return {...state, isFetching: true, isLoggedIn: false}
 
     case t.LOGIN_SUCCESS:
-      return {...state, isFetching: false, isAuthenticated: true,
+      return {...state, isFetching: false, isLoggedIn: true,
         username: action.username}
 
     case t.LOGIN_FAILURE:
-      return {...state, isFetching: false, isAuthenticated: false}
+      return {...state, isFetching: false, isLoggedIn: false}
 
     case t.LOGOUT_REQUEST:
-      return {...state, isFetching: true, isAuthenticated: false}
+      return {...state, isFetching: true, isLoggedIn: false}
 
     case t.LOGOUT_SUCCESS:
-      return {...state, isFetching: false, isAuthenticated: false}
+      return {...state, isFetching: false, isLoggedIn: false}
 
     default:
       return state
